235d0ad265f1677d59dfcdaaf888304238d50a7a
Adriano Dias <adrianodiasx93@gmail.com>
2017-03-27 18:55:18 -0300

    Adiciona regras fuzzy

--- Gaso/app/src/main/java/com/ericmguimaraes/gaso/util/FuzzyManager.java
+++ Gaso/app/src/main/java/com/ericmguimaraes/gaso/util/FuzzyManager.java
@@ -1,10 +1,15 @@
 package com.ericmguimaraes.gaso.util;
 
+import net.sourceforge.jFuzzyLogic.FIS;
+
 /**
  * Created by adrianodias on 3/19/17.
  */
 public class FuzzyManager {
     private static FuzzyManager ourInstance = new FuzzyManager();
+
+    private final String filename = "gaso.fcl";
+    private FIS fis;
 
     public static FuzzyManager getInstance() {
         return ourInstance;
@@ -12,4 +17,5 @@ public class FuzzyManager {
 
     private FuzzyManager() {
     }
+
 }
